2a633e655ab17aea7c6ab3ed27c8c5b70300851e
RayhaanK <rayhaankhan331@gmail.com>
2023-08-29 11:42:35 +0200

    added localhost

--- API/model/index.js
+++ API/model/index.js
@@ -1,8 +1,10 @@
 // index
 const Users = require('./users')
 const Products = require('./products')
+const Orders = require('./orders')
 // Export all objects
 module.exports = {
     users: new Users(),
-    products: new Products()
+    products: new Products(),
+    orders: new Orders()
 }
