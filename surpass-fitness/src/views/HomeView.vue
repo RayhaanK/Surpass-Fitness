33cbcc3dabdf0bfb323f8ae200400c168ca10b46
RayhaanK <rayhaankhan331@gmail.com>
2023-08-30 14:58:11 +0200

    added carousel, category and tip cards as well as iframe

--- surpass-fitness/src/views/HomeView.vue
+++ surpass-fitness/src/views/HomeView.vue
@@ -1,114 +1,122 @@
 <template>
   <div>
-    <section id="Home">
+    <section id="Home" class="home">
       <div class="landing">
-        <div
-          id="myCarousel"
-          class="carousel slide mb-6"
-          data-bs-ride="carousel"
-        >
-          <div class="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#myCarousel"
-              data-bs-slide-to="0"
-              class="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#myCarousel"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#myCarousel"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-          </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img
-                src="https://i.postimg.cc/y8x9QGTq/karsten-winegeart-0-Wra5-YYVQJE-unsplash.jpg"
-                class="d-block w-100 img-fluid cImg"
-                loading="lazy"
-                alt="..."
-              />
-              <div class="container">
-                <div class="carousel-caption text-end">
-                  <h1>One more for good measure.</h1>
-                  <p>
-                    Some representative placeholder content for the third slide
-                    of this carousel.
-                  </p>
-                  <p>
-                    <a href="#"><button class="btn1">More</button></a>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img
-                src="https://i.postimg.cc/kGqD67Fb/jelmer-assink-gze-Tj-Gu3b-k-unsplash.jpg"
-                class="d-block w-100 img-fluid cImg"
-                loading="lazy"
-                alt="..."
-              />
-              <div class="container">
-                <div class="carousel-caption text-end">
-                  <h1>One more for good measure.</h1>
-                  <p>
-                    Some representative placeholder content for the third slide
-                    of this carousel.
-                  </p>
-                  <p>
-                    <a href="#"><button class="btn1">More</button></a>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img
-                src="https://i.postimg.cc/hPCbBYcr/danielle-cerullo-CQf-Nt66tt-ZM-unsplash.jpg"
-                class="d-block w-100 img-fluid cImg"
-                loading="lazy"
-                alt="..."
-              />
-              <div class="container">
-                <div class="carousel-caption text-end">
-                  <h1>One more for good measure.</h1>
-                  <p>
-                    Some representative placeholder content for the third slide
-                    of this carousel.
-                  </p>
-                  <p>
-                    <a href="#"><button class="btn1">More</button></a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <button
-            class="carousel-control-prev"
-            type="button"
-            data-bs-target="#myCarousel"
-            data-bs-slide="prev"
+        <div class="container-fluid gx-0 contC">
+          <div
+            id="myCarousel"
+            class="carousel slide mb-6"
+            data-bs-ride="carousel"
           >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button
-            class="carousel-control-next"
-            type="button"
-            data-bs-target="#myCarousel"
-            data-bs-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
+            <div class="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#myCarousel"
+                data-bs-slide-to="0"
+                class="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#myCarousel"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#myCarousel"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              ></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img
+                  src="https://i.postimg.cc/y8x9QGTq/karsten-winegeart-0-Wra5-YYVQJE-unsplash.jpg"
+                  class="d-block w-100 img-fluid cImg"
+                  loading="lazy"
+                  alt="..."
+                />
+                <div class="container">
+                  <div class="carousel-caption text-end">
+                    <h1>One more for good measure.</h1>
+                    <p>
+                      Some representative placeholder content for the third
+                      slide of this carousel.
+                    </p>
+                    <p>
+                      <a href="#"><button class="btn1">More</button></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img
+                  src="https://i.postimg.cc/kGqD67Fb/jelmer-assink-gze-Tj-Gu3b-k-unsplash.jpg"
+                  class="d-block w-100 img-fluid cImg"
+                  loading="lazy"
+                  alt="..."
+                />
+                <div class="container">
+                  <div class="carousel-caption text-end">
+                    <h1>One more for good measure.</h1>
+                    <p>
+                      Some representative placeholder content for the third
+                      slide of this carousel.
+                    </p>
+                    <p>
+                      <a href="#"><button class="btn1">More</button></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img
+                  src="https://i.postimg.cc/hPCbBYcr/danielle-cerullo-CQf-Nt66tt-ZM-unsplash.jpg"
+                  class="d-block w-100 img-fluid cImg"
+                  loading="lazy"
+                  alt="..."
+                />
+                <div class="container">
+                  <div class="carousel-caption text-end">
+                    <h1>One more for good measure.</h1>
+                    <p>
+                      Some representative placeholder content for the third
+                      slide of this carousel.
+                    </p>
+                    <p>
+                      <a href="#"><button class="btn1">More</button></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#myCarousel"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#myCarousel"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
         </div>
       </div>
     </section>
@@ -122,25 +130,73 @@
           </p>
         </div>
         <div class="cards d-flex justify-content-center">
-          <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
+          <div class="row row-cols-1 row-cols-lg-3">
             <div class="col d-flex justify-content-center">
-              <div class="cardS">
-                <div class="cardS-info">
-                  <p class="title">Cat 1</p>
+              <div class="cardL-container">
+                <div class="cardL">
+                  <div class="img-content">
+                    <img
+                      src="https://i.postimg.cc/hPCbBYcr/danielle-cerullo-CQf-Nt66tt-ZM-unsplash.jpg"
+                      class="d-block img-fluid"
+                      loading="lazy"
+                      alt="..."
+                    />
+                  </div>
+                  <div class="content">
+                    <p class="heading">Card Hover</p>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipii voluptas
+                      ten mollitia pariatur odit, ab minus ratione adipisci
+                      accusamus vel est excepturi laboriosam magnam
+                      necessitatibus dignissimos molestias.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
             <div class="col d-flex justify-content-center">
-              <div class="cardS">
-                <div class="cardS-info">
-                  <p class="title">Cat 2</p>
+              <div class="cardL-container">
+                <div class="cardL">
+                  <div class="img-content">
+                    <img
+                      src="https://i.postimg.cc/hPCbBYcr/danielle-cerullo-CQf-Nt66tt-ZM-unsplash.jpg"
+                      class="d-block img-fluid"
+                      loading="lazy"
+                      alt="..."
+                    />
+                  </div>
+                  <div class="content">
+                    <p class="heading">Card Hover</p>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipii voluptas
+                      ten mollitia pariatur odit, ab minus ratione adipisci
+                      accusamus vel est excepturi laboriosam magnam
+                      necessitatibus dignissimos molestias.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
             <div class="col d-flex justify-content-center">
-              <div class="cardS">
-                <div class="cardS-info">
-                  <p class="title">Cat 3</p>
+              <div class="cardL-container">
+                <div class="cardL">
+                  <div class="img-content">
+                    <img
+                      src="https://i.postimg.cc/hPCbBYcr/danielle-cerullo-CQf-Nt66tt-ZM-unsplash.jpg"
+                      class="d-block img-fluid"
+                      loading="lazy"
+                      alt="..."
+                    />
+                  </div>
+                  <div class="content">
+                    <p class="heading">Card Hover</p>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipii voluptas
+                      ten mollitia pariatur odit, ab minus ratione adipisci
+                      accusamus vel est excepturi laboriosam magnam
+                      necessitatibus dignissimos molestias.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -149,30 +205,58 @@
       </div>
     </section>
     <section id="tips" class="tips mt-5">
-      <div class="container">
+      <div class="container-fluid textContainer">
         <div class="text">
-          <h2 class="mt-4">TIPS TO HELP YOU ON YOUR JOURNEY</h2>
+          <h2 class="mt-4 mb-3">TIPS TO HELP YOU ON YOUR JOURNEY</h2>
         </div>
       </div>
-      <div class="container-fluid  d-flex justify-content-center">
+        <div class="container-fluid d-flex justify-content-center tipContainer">
         <div class="row">
-          <div class="col">
-            <div class="card" style="width: 18rem">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">
-              Card subtitle
-            </h6>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+          <div class="col colT">
+            <div class="card tipC">
+              <div class="card-body">
+                <h2 class="card-title white">About</h2>
+                <h6 class="card-subtitle mb-2 white">Need some more information?</h6>
+                <p class="card-text white">
+                  Found about more about the comapny and team and frequently asked questions!
+                </p>
+                <a href="#" class="card-link"><button class="btn1">More</button></a>
+              </div>
+            </div>
+            <div class="card tipC">
+              <div class="card-body">
+                <h2 class="card-title mb-4 white">Contact Us</h2>
+                <h6 class="card-subtitle mb-2 white">Have any queries?</h6>
+                <p class="card-text white">
+                  Feel free to send us a message on the contact form
+                </p>
+                <a href="#" class="card-link"><button class="btn1">More</button></a>
+              </div>
+            </div>
+            <div class="card tipC cardLife">
+              <div class="card-body">
+                <h2 class="card-title white">Lift Off Life</h2>
+                <h6 class="card-subtitle mb-2 white">Looking for a personal trainer?</h6>
+                <p class="card-text white">
+                  Book a personal trainer to help you on your journey on the following website!
+                </p>
+                <a href="#" class="card-link"><button class="btn1">More</button></a>
+              </div>
+            </div>
           </div>
         </div>
-          </div>
-        </div>
+      </div>
+    </section>
+    <section id="Bmi" class="bmi">
+      <div class="container-fluid">
+        <h2 class="mt-4">CALCULATE YOUR BMI</h2>
+        <p class="subT">Don't know you BMI? Calculate it down below!</p>
+        <iframe
+          src="https://rayhaan-bmicalculator.netlify.app/"
+          frameborder="0"
+          width="100%"
+          height="500"
+        ></iframe>
       </div>
     </section>
   </div>
@@ -185,8 +269,12 @@ export default {
 </script>
 
 <style scoped>
+.carousel-item {
+  height: 82vh;
+}
+
 .cImg {
-  height: 90vh !important;
+  height: 82vh;
   object-fit: cover;
 }
 
@@ -215,37 +303,142 @@ h2 {
   transition: 0.2s;
 }
 
-.cardS {
+
+.category {
+  height: max-content;
+}
+.cardL-container {
   width: 400px;
   height: 360px;
-  padding: 5px;
-  overflow: visible;
   position: relative;
-  z-index: 1;
-  border: none;
 }
 
-.cardS-info {
-  background: #79031d;
-  color: #79031d;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.cardL-container::before {
+  z-index: -1;
+  position: absolute;
+  transform: translate3d(0, 0, 0) scale(0.95);
+  filter: blur(20px);
+}
+
+.cardL {
   width: 100%;
   height: 100%;
-  overflow: visible;
-  border-radius: 0.7rem;
+  border-radius: inherit;
+  overflow: hidden;
 }
 
-.cardS .title {
-  font-weight: bold;
-  font-size: 2rem;
-  letter-spacing: 0.1em;
+.cardL .img-content{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: scale 0.6s, rotate 0.6s, filter 1s;
+  fill: #e8e8e8;
+  transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
 }
 
-.cardS:hover .cardS-info {
-  color: #edb518;
-  transition: color 1s;
+.img-content img {
+  width: 400px;
+  height: 360px;
+  object-fit: cover;
+}
+
+.cardL .content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: #f5f7f7;
+  padding: 20px;
+  line-height: 1.5;
+  border-radius: 5px;
+  opacity: 0;
+  transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+}
+
+.cardL .content .heading {
+  font-size: 32px;
+  font-weight: 800;
+}
+
+.cardL:hover .content {
+  opacity: 1;
+}
+
+.cardL:hover .img-content {
+  scale: 2.5;
+  filter: blur(7px);
+  fill: transparent;
+}
+
+.tipContainer {
+  height: max-content;
+}
+
+.colT {
+  display: flex;
+}
+
+.tipsC {
+  display: flex;
+  justify-content: center;
+  height: 400px;
+}
+
+.tipC {
+  background-color: #79031d;
+  width: 100%;
+  height: 450px;
+  border-radius: none;
+}
+
+.white {
+  color: #f5f7f7;
+}
+
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: end ;
+}
+
+.cardLife {
+  background-image: url(https://i.postimg.cc/PJqYdhgj/Daniel-Gallego-1.png);
+  background-size: cover;
+  background-position: center;
+}
+
+.bmi {
+  height: 50vh;
+}
+
+@media (max-width: 992px) {
+  .colT {
+    display: flex;
+    flex-direction: column !important;
+  }
+
+.tips {
+  height: max-content ;
+}
+
+}
+@media (max-width: 850px) {
+  .cImg {
+    height: auto !important;
+  }
+
+  .carousel-item {
+    height: auto;
+  }
+
 }
 
 @media (max-width: 425px) {
@@ -256,6 +449,20 @@ h2 {
   .cardS-info {
     color: #edb518;
   }
+
+  .img-content img {
+  width: auto;
+  height: 360px;
+  object-fit: cover;
+}
+
+
+.cardL-container {
+  width: auto;
+  height: 360px;
+  position: relative;
+}
+
 }
 @media (max-width: 320px) {
   .cardS {
