65fca8b9d6b156b7ae1b2c979370558e7c57f12f
RayhaanK <rayhaankhan331@gmail.com>
2023-09-13 09:26:09 +0200

    updated userUpdate

--- API/model/users.js
+++ API/model/users.js
@@ -112,7 +112,7 @@ class Users {
             SET ?
             WHERE userID = ?;   
             `;
-    db.query(query, [data, req.params.id], (err) => {
+    db.query(query, [data], (err) => {
       if (err) throw err;
       res.json({
         status: res.statusCode,
